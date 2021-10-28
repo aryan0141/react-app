40e24d19f5fe7491b5fc2fba17a8461c0dc5cde0
Kartik-Aggarwal <66867025+Kartik-Aggarwal@users.noreply.github.com>
2021-10-28 12:33:56 +0530

    Update Header.js

--- src/My Components/Header.js
+++ src/My Components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import { TestimonialCard } from "./TestimonialCard";
 
-export const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export const Testimonials = () => {
+  return (
+    <div>
+      <Typography variant="h3">Testimonials</Typography>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <TestimonialCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
